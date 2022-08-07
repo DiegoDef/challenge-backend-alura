bd09a21fa77653365e4ee87205b77c5a31cb6bd2
Diego Rossi Corrêa <diegorossicorrea2000@gmail.com>
2022-08-07 14:21:28 -0300

    adiciona bean modelMapper

--- src/main/java/br/com/challenge/alura/FinancasApplication.java
+++ src/main/java/br/com/challenge/alura/FinancasApplication.java
@@ -1,10 +1,17 @@
 package br.com.challenge.alura;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FinancasApplication {
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinancasApplication.class, args);
